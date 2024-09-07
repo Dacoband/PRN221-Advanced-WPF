00ab8ec659aaceef5515919e01cbf5011080a2f4
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-09-07 16:35:35 +0700

    update

---
+++ PRN221_TrialTest/PRN221PE_FA22_TrialTest_HuynhThienNhan/CandidateManagement_HuynhThienNhan/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CandidateManagement_HuynhThienNhan
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
