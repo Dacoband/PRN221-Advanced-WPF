00e58072c6dfa203886792787e96e686aa00cffe
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-09-11 16:56:53 +0700

    add Interface connect DB

--- PRN221_TrialTest/PRN221PE_FA22_TrialTest_HuynhThienNhan/CandidateManagement_HuynhThienNhan/MainWindow.xaml.cs
+++ PRN221_TrialTest/PRN221PE_FA22_TrialTest_HuynhThienNhan/CandidateManagement_HuynhThienNhan/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Candidate_BusinessObjects.Models;
+using Candidate_Services;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,45 @@ namespace CandidateManagement_HuynhThienNhan
     /// </summary>
     public partial class MainWindow : Window
     {
+        private IHRAccountService iHRAccountService;
+        
         public MainWindow()
         {
             InitializeComponent();
+            iHRAccountService = new HRAccountService();
+        }
+
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult answer = MessageBox.Show("Do you want to exit the app?", "Exit App!", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (answer == MessageBoxResult.Yes)
+                Application.Current.Shutdown();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtEmail.Text) || string.IsNullOrEmpty(txtPassword.Password))
+            {
+                MessageBox.Show("Please enter email and password!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            Hraccount hraccount = iHRAccountService.GetHraccount(txtEmail.Text, txtPassword.Password);
+
+            if (hraccount == null)
+            {
+                MessageBox.Show("Login failed! Please check your email or password.", "Login Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (hraccount.MemberRole != 1)
+            {
+                MessageBox.Show("You do not have permission to access this function!", "Access Denied", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            this.Hide();
         }
     }
 }
---
+++ PRN221_TrialTest/PRN221PE_FA22_TrialTest_HuynhThienNhan/Candidate_Repositories/HRAccountRepo.cs
@@ -0,0 +1,15 @@
+﻿using Candidate_BusinessObjects.Models;
+using Candidate_DAOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Candidate_Repositories
+{
+    public class HRAccountRepo : IHRAccountRepo
+    {
+        public Hraccount GetHraccount(string email, string password) => HRAccountDAO.Instance.GetHraccount(email, password);
+    }
+}
